005805ba2b8f94c40e4e98b2f1f6a8c18e1124f4
rider <myintthwaykhine127@gmail.com>
2021-11-03 09:22:31 +0630

    add blade template in view folder unser resources folder/view folder and add new Route in routes folder

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
+});
+
+Route::get('about',function(){
+    return view('about');
+});
+
+Route::get('product',function(){
+    return view('product');
+});
+
+Route::get('contact',function(){
+    return view('contact');
+});
+
+Route::get('shop',function(){
+    return view('shop');
 });
